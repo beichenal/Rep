dcae7d562cc331a3c8cf3093fc45971867d58acf
beichenal <1615527807@qq.com>
2018-02-23 18:39:33 +0800

    基本完成

--- yuedu/js/articleList.js
+++ yuedu/js/articleList.js
@@ -12,7 +12,7 @@ myajax("GET", url + "/posts/list?page=1" + "&limit=3", function (data) {
         //end
         for (let i = 0; i < 3; i++) {
             var ul_li = document.createElement("li");
-            ul_li.innerHTML = dealwith_li(/*  static_url + data.data.articles[i].cover  */ "", /*  static_url + data.data.articles[i].cover */"", data.data.articles[i].title, data.data.articles[i].abstract, static_url + data.data.articles[i].author.avatar, data.data.articles[i].author.name, time(data.data.articles[i].create_time), data.data.articles[i].praise_sum, data.data.articles[i].look_sum, data.data.articles[i].author._id, data.data.articles[i]._id);
+            ul_li.innerHTML = dealwith_li( static_url + data.data.articles[i].cover, static_url + data.data.articles[i].cover, data.data.articles[i].title, data.data.articles[i].abstract, static_url + data.data.articles[i].author.avatar, data.data.articles[i].author.name, time(data.data.articles[i].create_time), data.data.articles[i].praise_sum, data.data.articles[i].look_sum, data.data.articles[i].author._id, data.data.articles[i]._id);
             UL.appendChild(ul_li);
         }
       //  lazyload();
