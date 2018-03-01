1b5054e057930c525f1fdae30bf948226c78b6c9
beichenal <1615527807@qq.com>
2018-03-01 09:06:51 +0800

    修改

---
+++ Hotel/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#city").click(function(){
+        $("#select-city").toggleClass('hidden');
+
+    })
+    $("#date").click(function(){
+        $("#date-tab").toggleClass("hidden");
+    })
+
+})
